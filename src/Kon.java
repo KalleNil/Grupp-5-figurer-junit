ba20fc4552f392713935dd658cda4fe400763f92
Preecha Häll <preecha.hall@gmail.com>
2021-04-14 15:12:37 +0200

    skapa kon update

--- src/Kon.java
+++ src/Kon.java
@@ -15,14 +15,15 @@ public class Kon {
     float slant_height;
     static float pi= (float) 3.14159;
     
-     public Kon(double radius, double height) {
+     public Kon(float radius, float height, float slant_height, float pi) {
         this.radius = (float) radius;
         this.height = (float) height;
         this.slant_height = slant_height;
+        Kon.pi = pi;
      }
      //Räkna konens sida "slant height" med s = math.sqrt (r^2 + h^2)
      //Använder pythagoras sats 
-     public static float slantHeight (float radius, float height, float slant_height) {
+     public static float slantHeight (float radius, float height, float slant_height, float pi) {
          
          return slant_height = (float) Math.sqrt(radius * radius + height * height);
      }
@@ -32,7 +33,7 @@ public class Kon {
          return (float)1 / 3 * pi * height * radius * radius;
      }
      //Räkna ytan av konen med pi * r * s + pi * r^2
-     public static float surfaceArea(float radius, float height, float slant_height) {
+     public static float surfaceArea(float radius, float height, float slant_height, float pi) {
         
         return pi * radius * slant_height  + pi * radius * radius;   
      }
