d0fb56f19cba81c45e99322288a2349089a6bca2
Preecha Häll <preecha.hall@gmail.com>
2021-04-16 17:59:48 +0200

    kon update TEST

---
+++ test/KonTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author preec
+ */
+public class KonTest {
+    
+    public KonTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of slantHeight method, of class Kon.
+     */
+   
+
+    /**
+     * Test of volume method, of class Kon.
+     */
+    @Test
+    public void testVolume() {
+        Kon testKon = new Kon(2, 4);
+        float expResult = 16.75514793395996F;
+        float result = testKon.volume();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+
+    /**
+     * Test of slantHeight method, of class Kon.
+     */
+    @Test
+    public void testSlantHeight() {
+        System.out.println("slantHeight");
+        
+        Kon testKon = new Kon(2, 4);
+        float expResult = 4.4721360206604F;
+        float result = testKon.slantHeight();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+
+    /**
+     * Test of surfaceArea method, of class Kon.
+     */
+    @Test
+    public void testSurfaceArea() {
+        System.out.println("surfaceArea");
+        Kon instance = new Kon(2, 4);
+        float expResult = 40.66559600830078F;
+        float result = instance.surfaceArea();
+        assertEquals(expResult, result, 0.0);
+        
+    }
+
+    
+}
