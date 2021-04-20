e3bb5ad6f0d22650776f5ce1aa6ddd8296db562f
denej <denej@DESKTOP-EI6861D>
2021-04-20 14:26:14 +0200

    Test

---
+++ Grupp5figurer/src/main/java/com/mycompany/grupp5figurer/Rektangel.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author denej
+ */
+public class Rektangel extends Figurer implements Geometri {
+    
+    public Rektangel(double bas, double höjd){
+        this.bas = bas;
+        this.höjd = höjd;
+    }
+
+    @Override
+    public double area() {
+        double area = bas * höjd;
+        return area;
+    }
+
+    @Override
+    public double omkrets() {
+        double omkrets = (bas * 2) + (höjd * 2);
+        return omkrets;
+    }
+    
+}
