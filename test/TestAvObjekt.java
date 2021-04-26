24a7d74759da954470b28fcb31cf74a5da235982
Kalle <domare2@hotmail.com>
2021-04-26 19:11:21 +0200

    TestAvObjekt imports correction

--- test/TestAvObjekt.java
+++ test/TestAvObjekt.java
@@ -3,11 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
