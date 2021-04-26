3be39c17811e49e69217bd40ad9d5fc36e52abf9
denej <victor.jaque.mte21lin@tucsweden.se>
2021-04-26 21:37:52 +0200

    JBehave klart

--- test/CirkelSteps.java
+++ test/CirkelSteps.java
@@ -6,8 +6,6 @@
 
 
 
-import org.jbehave.core.annotations.AfterScenario;
-import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
